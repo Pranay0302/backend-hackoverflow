9ccfb0ecc19b808529866166da506b5e4ac11ae2
sonu ishaq <sonuishaq67@gmail.com>
2021-01-15 10:03:01 +0530

    get done post has a problem

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import { todoRouter } from "./routes/todo"
-// import mongoose from 'mongoose';
-
 dotenv.config();
-
 const port = process.env.SERVER_PORT ?? 8080;
 const URI = process.env.MONGODB_URI ?? 'mongodb://localhost:27017/todo';
+const db = require('../node_modules/monk')(URI)
 const app = express();
 
 app.use(express.json());
 
 app.use(todoRouter)
-
-// mongoose.connect(URI, {
-//     useCreateIndex: true,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// }, () => {
-//     console.log("Connected to database");
-// })
-
 
 app.get("/", (req, res) => {
     res.send('Hello World');
--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,20 +1,29 @@
 import express, { Request, Response } from 'express';
+import { type } from 'os';
 import { IToDo } from '../models/IToDo';
-// import mongoose from 'mongoose';
-
-
+const URI = process.env.MONGODB_URI ?? 'mongodb://localhost:27017/todo';
+const db = require('monk')(URI)
 const router = express.Router();
+const todoDB = db.get('todo')
 
 router.get('/api/todo', (req: Request, res: Response) => {
-    const todo: IToDo = { title: 'get title', description: 'get description' };
-    console.log(todo)
-    // mongoose.;
-    return res.send('the todo');
+    const all = todoDB.find({}).then((docs: any) => {
+        console.log(docs)
+        return res.send(docs);
+    })
 })
 
 router.post('/api/todo', (req: Request, res: Response) => {
-    const todo: IToDo = { title: 'post title', description: 'post description' };
-    console.log(todo)
+    // console.log(req);
+    let data: IToDo
+    req.on('data', (chunk) => {
+        data = JSON.parse(chunk)
+    })
+    req.on('end', () => {
+        console.log(data)
+    })
+    console.log(typeof (data))
+    todoDB.insert({ title: data.title, description: data.description });
     return res.send('new todo created');
 })
 
