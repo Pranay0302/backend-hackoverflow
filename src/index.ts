b9fa3f334bc2da8db8f077ccbc853e9ea6708390
sonu ishaq <sonuishaq67@gmail.com>
2021-01-15 19:45:46 +0530

    dynamic port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { potPlacesRouter } from "./routes/potplaces";
 import { todoRouter } from "./routes/todo";
 const cors = require('cors');
 const { urlencoded } = require('body-parser');
-const port = process.env.SERVER_PORT ?? 8080;
+const port = process.env.PORT ?? 8080;
 const app = express();
 const server = require('http').createServer(app)
 // const io = require('socket.io')(server)
