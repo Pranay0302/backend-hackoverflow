1db07731ca1979b063d61c80fc2c98fa526361fe
sonu ishaq <sonuishaq67@gmail.com>
2021-01-15 16:03:03 +0530

    added socket.io

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import express from "express";
 import { todoRouter } from "./routes/todo"
 const port = process.env.SERVER_PORT ?? 8080;
 const app = express();
-
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
 
 app.use(express.json());
 
@@ -12,6 +13,10 @@ app.get("/", (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`server started at http://localhost:${port}`);
+// });
+
+server.listen(port,()=>{
+    console.log(`server started at http://localhost:${port}`)
+})
